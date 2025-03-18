0ff221e3c7f10a171973eb185f1c760c659d1b0b
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-18 16:08:07 +0530

    feat: update Vercel configuration, enhance .vercelignore, and refactor database URL

--- api/index.php
+++ api/index.php
@@ -4,14 +4,12 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Create Laravel app
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
+)->send();
 
 $kernel->terminate($request, $response);
