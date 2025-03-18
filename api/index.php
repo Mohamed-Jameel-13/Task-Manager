dd34b65d52726ebdbdcf169787643dd170245182
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-18 15:51:13 +0530

    feat: update Vercel configuration and remove Tailwind CSS plugin

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+
+// Require composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create Laravel app
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
