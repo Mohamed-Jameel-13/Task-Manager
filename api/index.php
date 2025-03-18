37d9b5182fffe4b3ffcaf5ae9514f6cf57a445ca
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-18 16:16:28 +0530

    feat: update Vercel configuration, enhance .vercelignore, and streamline API routing

--- api/index.php
+++ api/index.php
@@ -1,15 +1,4 @@
 <?php
 
-// Require composer autoloader
-require __DIR__ . '/../vendor/autoload.php';
-
-// Create Laravel app
-$app = require __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
