b26cbca5125821b25d4ce7820ceb1ac06bf46e48
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-19 19:56:14 +0530

    refactor: implement Kernel class for middleware management

---
+++ app/Http/Kernel.php
@@ -0,0 +1,32 @@
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        // ...existing code...
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            // ...existing code...
+        ],
+
+        'api' => [
+            // ...existing code...
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
+
