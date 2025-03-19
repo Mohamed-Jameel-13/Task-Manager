d86ee58bd0078bf72f1871b0e7233e48a5a72223
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-19 20:13:12 +0530

    refactor: clarify CSRF handling in task routes documentation

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
+// Use normal routing since CSRF exceptions are already defined in VerifyCsrfToken middleware
 Route::resource('tasks', \App\Http\Controllers\TaskController::class);
