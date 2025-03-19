ef797806e472787e1bafcb8e2a9692708fc18382
Mohamed Jameel <smartjameel823@gmail.com>
2025-03-19 20:16:11 +0530

    refactor: enhance CSRF token setup for jQuery in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+// For jQuery
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+// Or for Axios (already configured in Laravel by default)
+// axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
